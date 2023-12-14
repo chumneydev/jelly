2184d4eea48f9b6b39a9b74a668e98bfea710c0e
Louis <lstephens@chumneyads.com>
2023-12-14 11:17:37 -0500

    fix(tidal): narrows guide search to files with extension
    Fixes an issues with searching the guides directory. Hidden/system files like DS_Store were breaking the build pipeline

--- utilities/tidal/src/build/tidal.config.ts
+++ utilities/tidal/src/build/tidal.config.ts
@@ -4,9 +4,10 @@ import path from "path";
 const guidesDir = path.resolve(__dirname, "../guides");
 const guideFiles = fs.readdirSync(guidesDir);
 
-const guides = guideFiles.map((file) => {
-	const guide = require(path.join(guidesDir, file));
-	return guide.default;
-});
-
+const guides = guideFiles
+	.filter((file) => file.endsWith(".ts") || file.endsWith(".js"))
+	.map((file) => {
+		const guide = require(path.join(guidesDir, file));
+		return guide.default;
+	});
 export default guides;
