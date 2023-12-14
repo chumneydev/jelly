27153b5cc772a5a0344ebd87788b0ef13bc89417
Louis <lstephens@chumneyads.com>
2023-12-14 11:18:22 -0500

    fix(tidal): fixes a return of void
    Fixes the return of void within the function

--- utilities/tidal/src/lib/utils/createPrefixedDictionary.ts
+++ utilities/tidal/src/lib/utils/createPrefixedDictionary.ts
@@ -2,7 +2,7 @@ import { Dictionary } from "~types/dictionary";
 
 const createPrefixedDictionary = (property: string, dictionaries: Dictionary[]) => {
 	return dictionaries.flatMap((dictionary) => {
-		dictionary.dictionary.map((item) => ({
+		return dictionary.dictionary.map((item) => ({
 			name: `${property}-${item.name}`,
 			value: item.value,
 		}));
