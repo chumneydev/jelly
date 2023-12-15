eb49db1f9fd04c774b5847f9d37e77043301cff3
Louis <lstephens@chumneyads.com>
2023-12-15 16:58:09 -0500

    feat(tidal): outputs complete utility classes as const

---
+++ utilities/tidal/src/lib/conversions/compileClassList.ts
@@ -0,0 +1,74 @@
+import chalk from "chalk";
+
+import { checkIfDirectoryExists, createDirectory, createFileName, createFilePath, writeFile } from "~lib/utils/namesAndDirectoryHelper";
+
+import { Guide } from "~types/guide";
+
+const getColor = (color: string) => {
+	switch (color) {
+		case "red":
+			return chalk.red;
+		case "green":
+			return chalk.green;
+		case "magenta":
+			return chalk.magenta;
+		case "yellow":
+			return chalk.yellow;
+		case "blue":
+			return chalk.blue;
+		default:
+			return chalk.white;
+	}
+};
+
+const compileClassList = (guides: Guide[]) => {
+	const breakpoints = ["sm", "md", "lg", "xl"];
+	const currentClassList: string[] = [];
+
+	for (const guide of guides) {
+		const guideClassList: string[] = [];
+
+		for (const variant of guide.variants) {
+			for (const dictionaryItem of variant.classes) {
+				guideClassList.push(dictionaryItem.name);
+			}
+		}
+
+		// Add the classes for this guide to the main class list
+		currentClassList.push(...guideClassList);
+
+		// Then, add the breakpoint-prefixed classes for this guide
+		for (const breakpoint of breakpoints) {
+			for (const className of guideClassList) {
+				currentClassList.push(`${breakpoint}:${className}`);
+			}
+		}
+	}
+
+	return currentClassList;
+};
+
+const writeClassListToFile = async (classList: string[]) => {
+	const fileName = createFileName("currentUtilites", "ts", false, "");
+	const filePath = createFilePath(`classes`, "");
+
+	const classListString = `const currentUtilites = [\n${classList.map((className) => `  "${className}",`).join("\n")}\n];\n export default currentUtilites;`;
+
+	if (!(await checkIfDirectoryExists(filePath))) {
+		await createDirectory(filePath);
+	}
+
+	writeFile(filePath, fileName, classListString);
+};
+
+const createClassList = async (guides: Guide[], color: string) => {
+	console.log(getColor(color)(`Creating class list...`));
+
+	const classList = compileClassList(guides);
+
+	await writeClassListToFile(classList);
+
+	console.log(getColor(color)(`Finished creating class list!`));
+};
+
+export default createClassList;
--- utilities/tidal/src/lib/tasks/buildGuides.ts
+++ utilities/tidal/src/lib/tasks/buildGuides.ts
@@ -4,10 +4,12 @@ import convertGuides from "~lib/conversions/convertGuides";
 
 import transformSass from "~lib/transforms/transformSass";
 import transformTypescript from "~lib/transforms/transformTypescript";
+import createClassList from "~lib/conversions/compileClassList";
 
 const buildGuides = async (guides: Guide[]) => {
 	await convertGuides(guides, transformSass, "scss", "_", "blue");
 	await convertGuides(guides, transformTypescript, "ts", "", "yellow");
+	await createClassList(guides, "magenta");
 };
 
 export default buildGuides;
