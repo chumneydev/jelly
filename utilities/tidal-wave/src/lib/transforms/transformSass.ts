93e1524e290a08fb6cdf0dbc600556487f4984a9
Louis <lstephens@chumneyads.com>
2023-12-18 15:03:56 -0500

    fix(tw): removes space from generated map name

--- utilities/tidal-wave/src/lib/transforms/transformSass.ts
+++ utilities/tidal-wave/src/lib/transforms/transformSass.ts
@@ -3,7 +3,9 @@ import { ComponentGroup } from "~types/component";
 import camelToKebab from "~lib/utils/camelToKebab";
 
 const transfromSass = (group: ComponentGroup) => {
-	let sassMap = `$${group.name}: (\n`;
+	const groupName = group.name.replace(/\s/g, "-").toLowerCase();
+
+	let sassMap = `$${groupName}: (\n`;
 
 	group.components.forEach((component) => {
 		sassMap += `\t${component.className}: (\n`;
