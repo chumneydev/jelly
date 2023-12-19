a4d5ddfd705082b7f5bb74f1206fc1e3919c5fcf
Louis <lstephens@chumneyads.com>
2023-12-19 11:29:38 -0500

    feat(tw): adds transitions from tidal to buttons-simple

--- utilities/tidal-wave/src/components/buttons-simple.ts
+++ utilities/tidal-wave/src/components/buttons-simple.ts
@@ -2,6 +2,8 @@
 import colors from "@jelly/tidal/dist/guides/ts/colors";
 import layout from "@jelly/tidal/dist/guides/ts/layout";
 import flexboxGrid from "@jelly/tidal/dist/guides/ts/flexbox-grid";
+import transition from "@jelly/tidal/dist/guides/ts/transition";
+import typography from "@jelly/tidal/dist/guides/ts/typography";
 import spacing from "@jelly/tidal/dist/guides/ts/spacing";
 
 import { Component, ComponentGroup } from "~types/component";
@@ -11,17 +13,23 @@ const baseButton: Component = {
 	className: "jl-button",
 	base: {
 		display: layout.display.classes["flex"],
+		justifyContent: flexboxGrid.justifyContent.classes["justify-center"],
 		alignItems: flexboxGrid.alignItems.classes["items-center"],
 		color: colors.baseTextColors.classes["text-white"],
 		paddingTop: spacing.paddingTop.classes["pt-4"],
 		paddingBottom: spacing.paddingBottom.classes["pb-4"],
+		cursor: "pointer",
+		fontWeight: typography.fontWeight.classes["font-bold"],
+		transitionProperty: transition.transitionProperty.classes["transition-background"],
+		transitionDuration: transition.transitionDuration.classes["duration-200"],
+		transitionTimingFunction: transition.transitionTimingFunction.classes["ease-in-out"],
 	},
 
 	hover: {},
 };
 
 const simpleButtons: ComponentGroup = {
-	name: "Simple Buttons",
+	name: "Buttons Simple",
 	description: "Button components",
 	components: [],
 };
