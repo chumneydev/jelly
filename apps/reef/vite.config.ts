29c159a4d8708e4a08f22ffca860ff78f5ee2f80
Louis <lstephens@chumneyads.com>
2023-12-29 14:35:38 -0500

    feat(reef): adds zustand for global state management

--- apps/reef/vite.config.ts
+++ apps/reef/vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ command, mode }) => {
 				"@assets": path.resolve(__dirname, "./src/assets"),
 				"@components": path.resolve(__dirname, "./src/components"),
 				"@defaults": path.resolve(__dirname, "./src/defaults"),
+				"@stores": path.resolve(__dirname, "./src/stores"),
 				"@styles": path.resolve(__dirname, "./src/styles"),
 				"@context": path.resolve(__dirname, "./src/context"),
 				"~types": path.resolve(__dirname, "./src/types"),
