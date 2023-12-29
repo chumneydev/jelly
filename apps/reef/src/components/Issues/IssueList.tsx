d19d6b326fc84b6e25aed77a1a3adfbbe2f9dd03
Louis <lstephens@chumneyads.com>
2023-12-29 16:48:04 -0500

    feat(reef): adds max-h transition for animating list

--- apps/reef/src/components/Issues/IssueList.tsx
+++ apps/reef/src/components/Issues/IssueList.tsx
@@ -1,14 +1,18 @@
 import useIssuesStore from "@stores/issuesStore";
+import { clsx } from "clsx";
 
 interface IssueListProps {}
 
 const IssueList = ({}: IssueListProps) => {
 	const { issues, showIssues } = useIssuesStore();
 	return (
-		<div>
-			{issues.map((issue) => (
-				<div>{issue.title}</div>
-			))}
+		// <div className={clsx(showIssues ? "translate-y-0" : " translate-y-full", "transform transition-transform duration-500 ease-in-out")}>
+		// 	{issues.map((issue) => (
+		// 		<div>{issue.title}</div>
+		// 	))}
+		// </div>
+		<div className={clsx(showIssues ? "max-h-[1000px]" : " max-h-0", "transition-max-h overflow-hidden bg-rose-500 duration-1000 ease-in-out")}>
+			{showIssues && issues.map((issue) => <div>{issue.title}</div>)}
 		</div>
 	);
 };
--- apps/reef/src/components/Issues/IssueTracker.tsx
+++ apps/reef/src/components/Issues/IssueTracker.tsx
@@ -1,15 +1,13 @@
 import IssueButton from "@components/Issues/IssueButton";
 import IssueList from "@components/Issues/IssueList";
-import useIssuesStore from "@stores/issuesStore";
 
 interface IssueTrackerProps {}
 
 const IssueTracker = ({}: IssueTrackerProps) => {
-	const { showIssues } = useIssuesStore();
 	return (
 		<div class="absolute bottom-10 right-10 flex flex-col">
 			<IssueButton />
-			{showIssues && <IssueList />}
+			{<IssueList />}
 		</div>
 	);
 };
