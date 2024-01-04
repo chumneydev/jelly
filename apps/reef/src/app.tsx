86dbb01627e98de5c83616da3fb6083f3e440339
Louis <lstephens@chumneyads.com>
2024-01-04 09:55:52 -0500

    feat(reef): adds checkWarnings from store in apps useEffect

--- apps/reef/src/app.tsx
+++ apps/reef/src/app.tsx
@@ -1,14 +1,16 @@
 import { useEffect } from "preact/hooks";
 import "@styles/app.css";
+import useWarningStore from "@stores/warningStore";
 
 export function App() {
+	const { checkWarnings } = useWarningStore();
 	useEffect(() => {
-		console.log("Hello");
+		checkWarnings();
 	}, []);
 
 	return (
-		<>
+		<div class="fixed bottom-10 right-10 flex flex-col-reverse items-end gap-4">
 			<p>Hello</p>
-		</>
+		</div>
 	);
 }
